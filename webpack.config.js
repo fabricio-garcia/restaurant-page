64aa426e71ec4653c7774113cae1526a4d45b8af
Fabricio Garcia <gael.fabricio@gmail.com>
2020-03-02 23:18:39 -0600

    chore: Modify configuration files
    - Add scripts to package.json
    - Add dev dependencies to package.json
    - Modify webpack.config.js
    - Add babelrc config file
    
    Refs. #1, See also #2

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,22 @@
-const path = require('path');
+const path = require("path");
+const nodeExternals = require('webpack-node-externals');
 
 module.exports = {
-	entry: './src/index.js',
-	output: {
-		filename: 'main.js',
-		path: path.resolve(__dirname, 'dist'),
-	},
-}
+  target: "node",
+  entry: "./src/index.js",
+  output: {
+    filename: "main.js",
+    path: path.resolve(__dirname, "dist")
+  },
+  externals: [nodeExternals()],
+
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: "babel-loader"
+      }
+    ]
+  }
+};
