4986a2007f0f2e76372cf0ded446626816455935
Fabricio Garcia <gael.fabricio@gmail.com>
2020-03-03 23:05:09 -0600

    feat: DOM Manipulation
    - Define references to work with the DOM
    
    Refs. #2

--- src/page-load.js
+++ src/page-load.js
@@ -1,12 +1,42 @@
 const pageLoad = () => {
-  // const helloWorld = console.log('Hello World!');
+
   const content = document.getElementById('content');
-  const image = document.createElement('aside');
-  const mainContent = document.createElement('section');
+  const imageContainer = document.createElement('aside');
+  const mainContent = document.createElement('div');
   const navBar = document.createElement('nav');
   const ul = document.createElement('ul');
   const liHome = document.createElement('li');
   const aHome = document.createElement('a');
+  const liMenu = document.createElement('li');
+  const aMenu = document.createElement('a');
+  const liContact = document.createElement('li');
+  const aContact = document.createElement('a');
+  const restaurantInfo = document.createElement('section');
+  const footer = document.createElement('footer');
+
+  imageContainer.classList.add('restaurant-logo');
+  mainContent.classList.add('main-content');
+
+  aHome.id = 'home';
+  aHome.textContent = 'Home';
+  aMenu.id = 'menu';
+  aMenu.textContent = 'Menu';
+  aContact.id = 'contact';
+  aContact.textContent = 'Contact';
+  footer.textContent = 'Restaurant Page'
+
+  content.appendChild(imageContainer);
+  liHome.appendChild(aHome);
+  liMenu.appendChild(aMenu);
+  liContact.appendChild(aContact);
+  ul.appendChild(liHome);
+  ul.appendChild(liMenu);
+  ul.appendChild(liContact);
+  navBar.appendChild(ul);
+  mainContent.appendChild(navBar);
+  mainContent.appendChild(restaurantInfo);
+  content.appendChild(mainContent);
+  content.appendChild(footer);
 
 }
 
