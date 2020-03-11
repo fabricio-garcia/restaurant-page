4b2c1705807fb3ab8464e2c71527a3bbd95e903a
Fabricio Garcia <gael.fabricio@gmail.com>
2020-03-11 17:06:23 -0600

    feat: Add Contact Module
    - Add contact form code in contact js
    - Add style in a separate file called contact.css
    
    Refs. #2

--- src/modules/contact.js
+++ src/modules/contact.js
@@ -1,14 +1,42 @@
-const helloWorld = () => {
+const showContactInfo = () => {
   const content = document.getElementById('content');
   const mainContent = document.createElement('div');
-  const logo = document.createElement('div');
+  const contact = document.createElement('div');
 
   mainContent.classList.add('main-content');
-  logo.setAttribute('id', 'restaurant-logo');
-  logo.innerHTML = 'Text';
+  contact.setAttribute('id', 'contact-info');
+  contact.classList.add('center');
 
-  mainContent.appendChild(logo);
+  contact.innerHTML = `
+  <div class="container">
+  <form id="contact" action="" method="post">
+    <h3>Contact Us</h3>
+    <h4>Contact us today, and get reply with in 24 hours!</h4>
+    <fieldset>
+      <input placeholder="Your name" type="text" tabindex="1" required autofocus>
+    </fieldset>
+    <fieldset>
+      <input placeholder="Your Email Address" type="email" tabindex="2" required>
+    </fieldset>
+    <fieldset>
+      <input placeholder="Your Phone Number" type="tel" tabindex="3" required>
+    </fieldset>
+    <fieldset>
+      <textarea placeholder="Type your Message Here...." tabindex="5" required></textarea>
+    </fieldset>
+    <fieldset>
+      <button name="submit" type="submit" id="contact-submit" data-submit="...Sending">Submit</button>
+    </fieldset>
+  </form>
+</div>
+  `;
+
+  mainContent.appendChild(contact);
   content.appendChild(mainContent);
+
+  return {
+    showContactInfo,
+  };
 };
 
-export default helloWorld;
+export default showContactInfo;
